98fac8511ad076db0e458dd367bd68c1a46d2d12
Aemiro Mekete <=>
2023-05-09 23:53:16 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express();
+require('dotenv').config()
+app.post('/square-webhook',async(req, res)=>{
+    console.log('headers',req.headers)
+    console.log('body',req.body)
+    res.send('Welcome');
+})
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
