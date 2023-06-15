81d1a1c75ae666d7c12d22099dd68afee3ef32f2
Aemiro Mekete <=>
2023-06-16 00:51:49 +0300

    Minor change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express=require('express')
 const app=express();
 require('dotenv').config()
-app.post('/square-webhook',async(req, res)=>{
+app.post('/',async(req, res)=>{
     console.log('headers',req.headers)
     console.log('body',req.body)
     res.send('Welcome');
 })
-app.get('/square-webhook',async(req, res)=>{
+app.get('/',async(req, res)=>{
     console.log('headers',req.headers)
     console.log('body',req.body)
     res.send('Welcome');
