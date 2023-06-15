78377e356a981f402debd05536596adcc80dd59f
Aemiro Mekete <=>
2023-06-16 00:13:42 +0300

    Minor change

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.post('/square-webhook',async(req, res)=>{
     console.log('body',req.body)
     res.send('Welcome');
 })
+app.get('/square-webhook',async(req, res)=>{
+    console.log('headers',req.headers)
+    console.log('body',req.body)
+    res.send('Welcome');
+})
 const port=process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
