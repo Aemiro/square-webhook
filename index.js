3f9faac83696326e1fac6986220ccf8c6f8a8d96
Aemiro Mekete <=>
2023-06-22 13:28:38 +0300

    Minor change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express=require('express')
+const express=require('express');
+const getLocations = require('./square');
 const app=express();
 require('dotenv').config()
 app.post('/',async(req, res)=>{
@@ -8,8 +9,8 @@ app.post('/',async(req, res)=>{
 })
 app.get('/',async(req, res)=>{
     console.log('headers',req.headers)
-    console.log('body',req.body)
-    res.send('Welcome');
+    console.log('body',)
+    res.send(getLocations());
 })
 const port=process.env.PORT || 3000;
 app.listen(port, () => {
